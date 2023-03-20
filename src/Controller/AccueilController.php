6734b5d1b8a7281d2ecba52ed8d00616afceef5a
FakiriYassine <yassineyassine0012@gmail.com>
2023-03-20 12:21:06 +0100

    Phase 2 : premier test "Hello world !

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author intad
+ */
+class AccueilController {
+    /**
+     * @Route("/", name="accueil")
+     * @return Response
+     */
+    public function index(): Response{
+        return new Response("hello world");
+    }
+}
