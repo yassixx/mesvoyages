d616e76e9d609e8fe0fee66aed7eb8c12a3f56a3
FakiriYassine <yassineyassine0012@gmail.com>
2023-03-21 18:26:55 +0100

    Phase 14 : ORM (ajout d'un enregistrement).

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Visite;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,20 +17,22 @@ class VisiteType extends AbstractType
         $builder
             ->add('ville')
             ->add('pays')
-            ->add('datecreation', null, [
-                'widget'=>'single_text',
-                'label' => 'date'
+            ->add('datecreation', DateType::class, [
+                'widget' => 'single_text',
+                'data' => isset($options['data']) &&
+                    $options['data']->getDateCreation() != null ? $options['data']->getDateCreation() : new DateTime('now'),
+                'label' => 'Date'
             ])
             ->add('note')
             ->add('avis')
             ->add('tempmin', null, [
-                'label'=>'° min'
+                'label' => 't° min'
             ])
             ->add('tempmax', null, [
-                'label'=>'° max'
+                'label' => 't° max'
             ])
             ->add('submit', SubmitType::class, [
-                'label'=>'Enregistrer'
+                'label' => 'Enregistrer'
             ])
         ;
     }
