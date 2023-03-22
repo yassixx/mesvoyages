c3ed403259515f9819c74b8bf17b891b25bf2ed0
FakiriYassine <yassineyassine0012@gmail.com>
2023-03-22 10:37:43 +0100

    Phase 15 : ORM (deuxième table 'environnement' et lien entre les deux tables, type association ManyToMany).

--- migrations/Version20230322092322.php
+++ migrations/Version20230322092322.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230320144215 extends AbstractMigration
+final class Version20230322092322 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230320144215 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE visite ADD tempmin INT DEFAULT NULL, ADD tempmax INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE environnement (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE visite DROP tempmin, DROP tempmax');
+        $this->addSql('DROP TABLE environnement');
     }
 }
