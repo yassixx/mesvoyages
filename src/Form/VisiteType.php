aba93668d46ed9f82620398a68b6fd41fb802cb6
FakiriYassine <yassineyassine0012@gmail.com>
2023-04-06 17:38:13 +0200

    Phase 21 : sécurité

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class VisiteType extends AbstractType
 {
@@ -26,7 +27,12 @@ class VisiteType extends AbstractType
                     $options['data']->getDateCreation() != null ? $options['data']->getDateCreation() : new DateTime('now'),
                 'label' => 'Date'
             ])
-            ->add('note')
+            ->add('note', IntegerType::class, [
+                'attr' => [
+                    'min' => 0,
+                    'max' => 20
+                ]
+            ])
             ->add('avis')
             ->add('tempmin', null, [
                 'label' => 't° min'
